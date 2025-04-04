82b37cd8bed8e5ea8d2b599f68c338a75c74e7fa
Ehicodes <osaigbovoehinomen@gmail.com>
2025-04-04 23:00:57 +0100

    codingchallenge1. task1 and task2

---
+++ starter/codingchallenge.js
@@ -0,0 +1,29 @@
+'use strict';
+/*JavaScript in the Browser: DOM and Events
+Coding Challenge #1
+Implement a game resEt functionality, so that the player can make a new guess!
+Your tasks:
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the 'score' and
+'secretNumber' variables
+3. Restore the initial conditions of the message, number, score and guess input
+fields
+4. Also restore the original background color (#222) and number width (15rem)
+*/
+
+document.querySelector('.again').addEventListener('click', function () {
+  const secretNumber = Math.trunc(Math.random() * 20) + 1;
+  //score
+  const score = 20;
+  document.querySelector('.score').textContent = score;
+  //number
+  document.querySelector('.number').textContent = '?';
+  //message
+  document.querySelector('.message').textContent = 'Start guessing';
+  //backgroundColor
+  document.querySelector('body').style.backgroundColor = '#222';
+  //width
+  document.querySelector('.number').style.width = '15rem';
+  //guess input
+  document.querySelector('.guess').value = null;
+});
